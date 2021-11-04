d6f1c1a0a828c2d92454ac91898bff0f2ede2f7e
Nicolas Rios <dnicolas.rios98@gmail.com>
2021-11-04 20:53:16 +0100

    Add examples of useRef hook

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RealExampleRef } from './components/05-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/Layout';
 import './styles/styles.css';
 // import { FocusScreen } from './components/05-useRef/FocusScreen';
 // import { HookApp } from './HookApp';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
-ReactDOM.render(<RealExampleRef />, document.getElementById('root'));
+ReactDOM.render(<Layout />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
