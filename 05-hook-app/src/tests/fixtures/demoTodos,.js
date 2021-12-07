4ea78b17bb563527486c21eda5920c3f06d41de5
Nicolas Rios <nicolas.rios@icareweb.com>
2021-12-07 18:37:22 +0100

    Add fixtures dir to manage static data for test

---
+++ 05-hook-app/src/tests/fixtures/demoTodos,.js
@@ -0,0 +1,18 @@
+export const demoTodos = [
+	{
+		id: 1,
+		desc: 'Learn react',
+		done: false,
+	},
+	{
+		id: 2,
+		desc: 'Learn design patterns',
+		done: false,
+	},
+];
+
+export const newTodo = {
+	id: 3,
+	desc: 'Learn linux',
+	done: false,
+};
